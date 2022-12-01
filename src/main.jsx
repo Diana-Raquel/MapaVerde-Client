2f2f54e7b6ff0d53b6f4681e5e33788ecdd9f186
Diana-Raquel <00096520@uca.edu.sv>
2022-12-01 06:20:36 -0600

    edit:main

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 import { ConfigProvider } from './contexts/ConfigContex';
-
+import { UserContextProvider } from './contexts/UserContext';
+import { ToastContainer } from 'react-toastify';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+
 
 import axios from 'axios';
+import './index.css';
 
-import { ToastContainer } from 'react-toastify';
+
 import "react-toastify/dist/ReactToastify.css";
 
 axios.defaults.baseURL = import.meta.env.VITE_APIENDPOINT || "https://mapaverde-api-production.up.railway.app/api";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter>
-   <ConfigProvider>
-      <App />
-      <ToastContainer theme='dark' position='bottom-right' />
-      <LoadingSpinner />
-    </ConfigProvider>
-   </BrowserRouter>
+    <BrowserRouter>
+      <ConfigProvider>
+        <UserContextProvider>
+          <App />
+          <ToastContainer theme='dark' position='bottom-right' />
+          <LoadingSpinner />
+        </UserContextProvider>
+      </ConfigProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
