48d6c9dd454ee23e561dcc9e17d6d94a19f22ce8
Diana-Raquel <00096520@uca.edu.sv>
2022-12-01 06:06:30 -0600

    edit:login

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,29 @@
-import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
-import { ConfigProvider } from './contexts/ConfigContex';
-import { UserContexProvider } from './contexts/UserContext';
-import { ToastContainer } from 'react-toastify';
-import { BrowserRouter } from 'react-router-dom';
-
-import axios from 'axios';
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { ConfigProvider } from './contexts/ConfigContex';
+
+
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
+
+import axios from 'axios';
+
+import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-
-
-
-
 
 axios.defaults.baseURL = import.meta.env.VITE_APIENDPOINT || "https://mapaverde-api-production.up.railway.app/api";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ConfigProvider>
-        <UserContexProvider>
-          <App />
-          <ToastContainer theme='dark' position='bottom-right' />
-          <LoadingSpinner />
-        </UserContexProvider>
-      </ConfigProvider>
-    </BrowserRouter>
+   <BrowserRouter>
+   <ConfigProvider>
+      <App />
+      <ToastContainer theme='dark' position='bottom-right' />
+      <LoadingSpinner />
+    </ConfigProvider>
+   </BrowserRouter>
   </React.StrictMode>
 )
